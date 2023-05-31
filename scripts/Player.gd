6dff9296bb73726fbe86c14b9e72c9356e6044ca
dj-commits <donaldrjones90@gmail.com>
2023-05-30 20:55:19 -0500

    Added new sprites, updated zombie logic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,17 +3,19 @@ extends Node2D
 
 @export var move_speed: float;
 
+signal damaged;
 
 func _ready():
 	pass
 	
 func _process(delta):
-	position += getMovement() * move_speed * delta;
+	position += get_movement() * move_speed * delta;
 
 
-func getMovement() -> Vector2:
+func get_movement() -> Vector2:
 	var movement: Vector2;
 	movement = Input.get_vector("move_left", "move_right", "move_up", "move_down");
 	if(movement.length() > 0):
 		movement = movement.normalized();
+	$AnimationPlayer/AnimationTree["parameters/blend_position"] = movement;
 	return movement;
--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -2,39 +2,102 @@ extends Node2D
 
 
 @export var move_speed: float;
+@export var attackRange: float;
+@export var state: ZombieState;
 
+enum ZombieState { Idle, Walking, Possessed, Chasing, Attacking, Dying }
+
+
+var health: int;
+var atkDamage: int;
 var player_in_view: bool;
 var player: Node2D;
+var walk_destination: Vector2;
+var screen_size;
+
+@onready var walk_timer: Timer = $WalkTimer;
+
 signal player_seen;
+signal walk_dest_reached;
+
+
+
 
 func _ready():
-	pass
+	screen_size = get_viewport_rect().size;
 
 func _process(delta):
-	position += getMovement() * move_speed * delta;
-
-
-func getMovement() -> Vector2:
 	var movement: Vector2;
-	if(player_in_view):
-		movement = global_position.direction_to(player.global_position);
-		$AnimationPlayer/AnimationTree["parameters/blend_position"] = movement;
-		#print_debug(movement);
-	return movement;
+	match state:
+		ZombieState.Idle:
+			print_debug("Zombie Idle");
+			if(walk_timer.is_stopped()):
+				set_state(ZombieState.Walking);
+				walk_destination = Vector2(randf_range((screen_size.x-global_position.x), screen_size.x), randf_range((screen_size.y-global_position.y), screen_size.y));
+			else:
+				print_debug(walk_timer.time_left);
+		ZombieState.Walking:
+			print_debug("Zombie Walking");
+			position = global_position.move_toward(walk_destination, move_speed * delta).normalized();
+			check_distance_to_walk_destination();
+			print_debug(position);
+			print_debug(walk_destination);
+		ZombieState.Possessed:
+			print_debug("Zombie Possessed");
+		ZombieState.Chasing:
+			movement = global_position.direction_to(player.global_position);
+			print_debug("Zombie Chasing");
+		ZombieState.Attacking:
+			print_debug("Zombie Attacking");
+		ZombieState.Dying:
+			print_debug("Zombie Dying");
+		
+	
+	#position += movement * move_speed * delta;
+	$AnimationPlayer/AnimationTree["parameters/blend_position"] = movement;
+		
 
 
-func _on_area_2d_area_entered(area: Area2D):
+func check_distance_to_walk_destination():
+	# Checks if the zombie needs to continue walking and signals when reaching destination
+	if(ZombieState.Walking):
+		if(walk_destination != null):
+			var distance: float;
+			distance = global_position.distance_to(walk_destination);
+			if(distance <= 50 ):
+				walk_dest_reached.emit();
+			else:
+				print_debug("Distance: {distance}".format({"distance" : distance}) );
+				
+
+
+func get_state() -> ZombieState:
+	return state;
+
+
+func set_state(next_state: ZombieState):
+	state = next_state;
+	
+func _on_detection_zone__area_exited(area):
+	if(player != null):
+		player == null;
+	player_in_view = false;
+	set_state(ZombieState.Idle);
+
+
+func _on_detection_zone_area_entered(area):
 	player = area;
 	print_debug(area.position.dot(position));
 	if(area.position.dot(position) >= 0):
 		player_seen.emit();
 		player_in_view = true;
+		set_state(ZombieState.Chasing);
 	else:
-		print("Not looking at player"); # Replace with function body.
+		print("Not looking at player");
 
 
-func _on_area_2d_area_exited(area):
-	if(player != null):
-		player == null;
-	player_in_view = false;
-	#print_debug("player out of area"); # Replace with function body.
+func _on_walk_dest_reached():
+	print_debug("Walk Destination Reached");
+	walk_destination == null;
+	set_state(ZombieState.Idle);
+	walk_timer.start();
